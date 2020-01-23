22eea0060a8277380ee18723e9768cd8ceccd713
José Pineda <pinedaborges.josealejandro@gmail.com>
2020-01-23 10:04:45 -0400

    Other try

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,9 +8,10 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+        $response->header('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 }
