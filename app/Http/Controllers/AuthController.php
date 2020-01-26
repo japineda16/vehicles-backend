7acc67a09c9d7248a4474ec62922c96005045a9d
Jose <Pineda>
2020-01-26 13:41:29 -0400

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
             'name'     => $request->name,
             'email'    => $request->email,
             'password' => bcrypt($request->password),
-            'type' => null
+            'type' => 2
         ]);
         $user->save();
         return response()->json([
